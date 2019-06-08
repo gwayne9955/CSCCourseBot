f147913da1d8a5af944b98f7b96de3fb3ed8bfeb
Austin Silveria <austinsilveria@gmail.com>
2019-06-08 13:43:20 -0700

    Topic extraction from course descriptions

--- storage/DBPublisher.py
+++ storage/DBPublisher.py
@@ -23,6 +23,9 @@ class DBPublisher:
         self.NEXT_QUARTER = "next_schedule"
         self.NEXT_QUARTER_SQL = None
 
+        self.TOPICS = "course_topics"
+        self.TOPICS_SQL = None
+
         self.set_table_prefix("")
 
     def set_table_prefix(self, prefix: str) -> None:
@@ -47,6 +50,10 @@ class DBPublisher:
         self.NEXT_QUARTER_SQL = "INSERT INTO " + self.NEXT_QUARTER + \
                                 " (code, first, last, time) VALUES (%s, %s, %s, %s)"
 
+        self.TOPICS = prefix + self.TOPICS
+        self.TOPICS_SQL = "INSERT INTO " + self.TOPICS + " (code, topic) " \
+                          "VALUES (%s, %s)"
+
     def cleanup(self):
         self.db.truncate(self.MAIN)
         self.db.truncate(self.TERM)
@@ -56,6 +63,11 @@ class DBPublisher:
 
     def publish_catalog(self, courses: List[Course]) -> None:
         results = []
+
+        # topic_to_courses unused as of now, but if we want association tables
+        # in memory instead of database we have it here for fast lookup
+        course_to_topics, topic_to_courses = self.get_topics(courses)
+
         for course in courses:
             prereqs = "" if course.prereqs is None else course.prereqs
             data = (course.code.number, self.to_lower(course.name), course.units.min,
@@ -71,6 +83,50 @@ class DBPublisher:
                 data = (course.code.number, self.to_lower(dept))
                 results.append(self.db.store(self.DEPT_SQL, data))
 
+            for topic in course_to_topics[course.code.number]:
+                data = (course.code.number, topic)
+                results.append(self.db.store(self.TOPICS_SQL, data))
+
+    def publish_schedule(self, courses: Tuple[Dict[int, List[Tuple[str, str]]],
+                                              Dict[int, List[Tuple[str, str]]]]):
+        cur_courses = courses[0]
+        next_courses = courses[1]
+
+        self.publish_quarter(cur_courses, self.CUR_QUARTER_SQL)
+        self.publish_quarter(next_courses, self.NEXT_QUARTER_SQL)
+
+    @staticmethod
+    def get_topics(courses: List[Course]) -> Tuple[Dict[int: List[str]],
+                                                   Dict[str: List[int]]]:
+        """
+
+        :param courses: List of course objects that contains the descriptions
+                        and course numbers this method uses
+        :return course_to_topics: Maps course number to its list of topics
+        :return topic_to_courses: Maps topic to list of courses that reference it
+        """
+        NGRAMS = 3
+        course_to_topics = {}
+        topic_to_courses = {}
+
+        for course in courses:
+            word_list = course.desc.split(' ')
+            for num_words in range(NGRAMS):
+                for j in range(len(word_list) - num_words + 1):
+                    ngram = '_'.join(word_list[j:j+num_words])
+                    try:
+                        course_to_topics[course.code.number].add(ngram)
+                    except KeyError:
+                        course_to_topics[course.code.number] = set()
+                        course_to_topics[course.code.number].add(ngram)
+                    try:
+                        topic_to_courses[ngram].add(course.code.number)
+                    except KeyError:
+                        course_to_topics[ngram] = set()
+                        course_to_topics[ngram].add(course.code.number)
+
+        return course_to_topics, topic_to_courses
+
     def publish_quarter(self, quarter: Dict[int, List[Tuple[str, str]]], sql: str):
         results = []
         for code, sections in quarter.items():
@@ -82,14 +138,6 @@ class DBPublisher:
                         self.to_lower(name[-1]),
                         section[1])
                 results.append(self.db.store(sql, data))
-
-    def publish_schedule(self, courses: Tuple[Dict[int, List[Tuple[str, str]]],
-                                              Dict[int, List[Tuple[str, str]]]]):
-        cur_courses = courses[0]
-        next_courses = courses[1]
-
-        self.publish_quarter(cur_courses, self.CUR_QUARTER_SQL)
-        self.publish_quarter(next_courses, self.NEXT_QUARTER_SQL)
 
     @staticmethod
     def to_lower(string):
