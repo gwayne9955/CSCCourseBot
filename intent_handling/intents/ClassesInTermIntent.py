4c00ab922b191bd553b90806b4ddcb5d2bc0037c
mpangburn <mjpangbu@calpoly.edu>
2019-06-09 15:50:44 -0700

    Fix mixup in execute() methods

--- intent_handling/intents/ClassesInTermIntent.py
+++ intent_handling/intents/ClassesInTermIntent.py
@@ -8,4 +8,13 @@ class ClassesInTermIntent:
         self.parameters = parameters
 
     def execute(self, db):
-        pass
+        sql = 'SELECT term ' \
+              'FROM main_courses JOIN course_terms ON main_courses.code=course_terms.code ' \
+              'WHERE main_courses.name="{}"'.format(self.parameters.class_name)
+        result = db.call(sql)
+        if len(result) == 0:
+            return Signal.UNKNOWN, 'No course term information for {} is available.'.format(
+                self.parameters.class_name)
+        quarters = ', '.join(result)
+        return Signal.NORMAL, '{} is offered in {}.'.format(
+            self.parameters.class_name, quarters)
