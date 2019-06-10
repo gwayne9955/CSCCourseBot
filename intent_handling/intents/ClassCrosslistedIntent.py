64740f4860bfe2bb5ada3b0fe2c081d17995f3f6
mpangburn <mjpangbu@calpoly.edu>
2019-06-10 10:34:55 -0700

    Fix unique depts

--- intent_handling/intents/ClassCrosslistedIntent.py
+++ intent_handling/intents/ClassCrosslistedIntent.py
@@ -15,9 +15,13 @@ class ClassCrosslistedIntent:
         if len(result) == 0:
             return Signal.UNKNOWN, 'No information available for {}.'.format(self.parameters.class_name)
         else:
+            uniques = []
+            for code, dept in result:
+                if dept not in uniques:
+                    uniques.append((code, dept))
             code_string = db.course_code(self.parameters.class_name)
-            if len(result) == 1:
+            if len(uniques) == 1:
                 return Signal.NORMAL, '{} is not crosslisted in any other departments.'.format(code_string)
             else:
-                crosslisted = ', '.join('{} {}'.format(tup[1].upper(), tup[0]) for tup in result)
+                crosslisted = ', '.join('{} {}'.format(tup[1].upper(), tup[0]) for tup in uniques)
                 return Signal.NORMAL, '{} is crosslisted as {}.'.format(code_string, crosslisted)
