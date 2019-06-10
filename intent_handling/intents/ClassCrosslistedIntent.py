cf9fdb3dde27473c84d09c98541c233fcba59a58
mpangburn <mjpangbu@calpoly.edu>
2019-06-10 10:36:10 -0700

    Fix formatting

--- intent_handling/intents/ClassCrosslistedIntent.py
+++ intent_handling/intents/ClassCrosslistedIntent.py
@@ -11,17 +11,13 @@ class ClassCrosslistedIntent:
         sql = 'SELECT course_depts.code, department ' \
               'FROM course_depts JOIN main_courses ON course_depts.code=main_courses.code ' \
               'WHERE intent_name="{}"'.format(self.parameters.class_name)
-        result = db.call(sql)
+        result = [(code, dept) for code, dept in db.call(sql) if dept != 'csc']
         if len(result) == 0:
             return Signal.UNKNOWN, 'No information available for {}.'.format(self.parameters.class_name)
         else:
-            uniques = []
-            for code, dept in result:
-                if dept not in uniques:
-                    uniques.append((code, dept))
             code_string = db.course_code(self.parameters.class_name)
-            if len(uniques) == 1:
+            if len(result) == 1:
                 return Signal.NORMAL, '{} is not crosslisted in any other departments.'.format(code_string)
             else:
-                crosslisted = ', '.join('{} {}'.format(tup[1].upper(), tup[0]) for tup in uniques)
+                crosslisted = ', '.join('{} {}'.format(tup[1].upper(), tup[0]) for tup in result)
                 return Signal.NORMAL, '{} is crosslisted as {}.'.format(code_string, crosslisted)
