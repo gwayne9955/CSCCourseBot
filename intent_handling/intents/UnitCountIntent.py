dfd9fac0a5cef42836636afe612825eb068bbbcf
mpangburn <mjpangbu@calpoly.edu>
2019-06-10 09:46:02 -0700

    Fix typo

--- intent_handling/intents/UnitCountIntent.py
+++ intent_handling/intents/UnitCountIntent.py
@@ -11,7 +11,7 @@ class UnitCountIntent:
         sql = 'SELECT code, min_units, max_units ' \
               'FROM main_courses ' \
               'WHERE intent_name={}'.format(self.parameters.class_name)
-        result = db.execute(sql)
+        result = db.call(sql)
         if len(result) == 0:
             return Signal.UNKNOWN, 'No unit information for {} available.'.format(self.parameters.class_name)
         code, min_units, max_units = result[0]
