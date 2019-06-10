ec7f6f3f4365acff7c6b2e2d06f3b33ce0f87c22
mpangburn <mjpangbu@calpoly.edu>
2019-06-09 20:20:35 -0700

    Fix standing parsing

--- intent_handling/intents/ClassesWithStandingIntent.py
+++ intent_handling/intents/ClassesWithStandingIntent.py
@@ -11,7 +11,7 @@ class ClassesWithStandingIntent:
         sql = 'SELECT code, prereqs FROM main_courses'
         result = db.call(sql)
         matching = ['CSC {}'.format(c) for c, prereqs in result
-                    if self.parameters.class_qualification in prereqs.lower()]
+                    if '{} standing'.format(self.parameters.class_qualification) in prereqs.lower()]
         if len(matching) == 0:
             return Signal.NORMAL, 'No classes require {} standing.'.format(self.parameters.class_qualification)
         return Signal.NORMAL, 'The following classes require {} standing: {}.'.format(
