de607cf57d6822cd25fe12c371ca97cc3a1968e2
austinsilveria <austinsilveria@gmail.com>
2019-06-10 10:43:19 -0700

    ClassWithTopic and ClassesWithTopic

--- intent_handling/intents/ClassWithTopicIntent.py
+++ intent_handling/intents/ClassWithTopicIntent.py
@@ -8,28 +8,30 @@ class ClassWithTopicIntent:
         self.parameters = parameters
 
     def execute(self, db):
+        pretty_topic = ' '.join(self.parameters.subject_matter.split('_'))
         code = db.course_code(self.parameters.class_name)
-        sql = 'SELECT code' \
-              'FROM course_topics' \
-              'WHERE topic={}'.format(self.parameters.subject_matter)
+        sql = 'SELECT code ' \
+              'FROM course_topics ' \
+              'WHERE topic="{}"'.format(self.parameters.subject_matter)
         result = db.call(sql)
 
         codes = [row[0] for row in result]
-        if code.split(' ')[-1] not in codes:
+        print("Codes: {}, searching for: {}".format(codes, code.split(' ')[-1]))
+        if int(code.split(' ')[-1]) not in codes:
             output = 'No, {} does not cover {}.'.format(
-                code, self.parameters.subject_matter)
+                code, pretty_topic)
             if len(codes) > 0:
                 output += ' But, {} is covered in the following classes:\n'.format(
-                    self.parameters.subject_matter)
+                    pretty_topic)
                 for c in codes:
                     output += '\tCSC {}\n'.format(c)
             return Signal.UNKNOWN, output
 
         output = 'Yes, {} covers {}.'.format(code,
-                                             self.parameters.subject_matter)
+                                             pretty_topic)
         if len(codes) > 1:
             output += ' {} is covered in the following classes:\n'.format(
-                self.parameters.subject_matter)
+                pretty_topic)
             for c in codes:
                 output += '\tCSC {}\n'.format(c)
         return Signal.NORMAL, output
--- intent_handling/intents/ClassesWithTopicIntent.py
+++ intent_handling/intents/ClassesWithTopicIntent.py
@@ -8,16 +8,17 @@ class ClassesWithTopicIntent:
         self.parameters = parameters
 
     def execute(self, db):
+        pretty_topic = ' '.join(self.parameters.subject_matter.split('_'))
         sql = 'SELECT code ' \
               'FROM course_topics ' \
               'WHERE topic="{}"'.format(self.parameters.subject_matter)
         result = db.call(sql)
         if len(result) == 0:
             return Signal.UNKNOWN, 'No classes found with topic {}'.format(
-                self.parameters.subject_matter)
+                pretty_topic)
 
         output = "The following classes are related to {}:\n\n".format(
-            self.parameters.subject_matter)
+            pretty_topic)
         for course in result:
             output += "\tCSC {}\n".format(course[0])
 
