d709822674b790d3d9b6d3730baf76c0ef44e8d2
mpangburn <mjpangbu@calpoly.edu>
2019-06-09 18:48:54 -0700

    Fix type error

--- intent_handling/intents/TermAllUpperIntent.py
+++ intent_handling/intents/TermAllUpperIntent.py
@@ -12,5 +12,5 @@ class TermAllUpperIntent:
         result = db.call(sql)
         if len(result) == 0:
             return Signal.UNKNOWN, 'No upper division course data available for {}.'.format(self.parameters.quarter)
-        courses = ', '.join(tup[0] for tup in result)
+        courses = ', '.join('CSC {}'.format(tup[0]) for tup in result)
         return Signal.NORMAL, 'The following upper division courses are offered in {}: {}.'.format(self.parameters.quarter, courses)
