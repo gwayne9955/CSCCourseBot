89bcead6dfa65e1bc983c271fc3e9819dc7b7122
mpangburn <mjpangbu@calpoly.edu>
2019-06-09 18:38:16 -0700

    Lowercase quarter from parameters

--- intent_handling/parameters.py
+++ intent_handling/parameters.py
@@ -9,7 +9,7 @@ class Parameters:
         self.class_name = safe_get(d, 'CLASS_NAME')
         self.quarter = safe_get(d, 'QUARTER')
         if self.quarter is not None:
-            self.quarter = ''.join(c for c in self.quarter if c not in '!?.,')
+            self.quarter = ''.join(c for c in self.quarter if c not in '!?.,').lower()
         self.class_qualification = safe_get(d, 'CLASS_QUALIFICATION')
         self.department_abbreviation = safe_get(d, 'DEPARTMENT_ABBREVIATION')
         self.subject_matter = safe_get(d, 'SUBJECT_MATTER')
